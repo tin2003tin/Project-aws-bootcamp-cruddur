dd1aeb3c869d60b87d7dbde887307c8bfbf76ac4
tin2003tin Thongon <tinsiriwid@gmail.com>
2023-12-22 06:46:58 +0000

    add jwt

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -13,6 +13,8 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
+
 # HoneyComb ---------
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
@@ -78,6 +80,13 @@ cors = CORS(
   methods="OPTIONS,GET,HEAD,POST"
 )
 
+cognito_jwt_token = CognitoJwtToken(
+  user_pool_id=os.getenv("AWS_COGNITO_USER_POOL_ID"), 
+  user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+  region=os.getenv("AWS_DEFAULT_REGION")
+)
+
+
 # cloudwatch logs ----
 # @app.after_request
 # def after_request(response):
@@ -141,8 +150,24 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
+  access_token = extract_access_token(request.headers)
+  print("access_token",access_token)
+  try:
+    print("test1")
+    claims = cognito_jwt_token.verify(access_token)
+    # authenicatied request
+    # app.logger.debug("authenicated")
+    # app.logger.debug(claims)
+    # app.logger.debug(claims['username'])
+    print("claims",claims)
+    data = HomeActivities.run(cognito_user_id=claims['username'])
+  except TokenVerifyError as e:
+    # unauthenicatied request
+    # app.logger.debug(e)
+    # app.logger.debug("unauthenicated")
+    data = HomeActivities.run()
   return data, 200
+
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
 def data_handle(handle):
