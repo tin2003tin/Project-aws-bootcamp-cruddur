09c6451d5368c7e473317f80f869f292bb987a34
tin2003tin Thongon <tinsiriwid@gmail.com>
2023-12-24 16:04:50 +0000

    commit it right now !!!!!!!!!!!!!

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -192,7 +192,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'andrewbrown'
+  user_handle  = 'tin2003tin'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
