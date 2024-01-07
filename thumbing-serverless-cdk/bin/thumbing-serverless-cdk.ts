0c5ee9e0c50e4f269edc7485ec7a9b49e78f958f
tin2003tin Thongon <tinsiriwid@gmail.com>
2024-01-07 20:35:25 +0000

    commit now! (add cdk)

--- thumbing-serverless-cdk/bin/thumbing-serverless-cdk.ts
+++ thumbing-serverless-cdk/bin/thumbing-serverless-cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { ThumbingServerlessCdkStack } from '../lib/thumbing-serverless-cdk-stack';
+import { ThumbingServerlessCdkStack } from '../lib/thumbing-serverless-cdk';
 
 const app = new cdk.App();
 new ThumbingServerlessCdkStack(app, 'ThumbingServerlessCdkStack', {
